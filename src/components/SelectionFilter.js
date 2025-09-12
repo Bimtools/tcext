44f906894b79d48c1c81f2262fd8383647b63053
van-tuan.nguyen <van-tuan.nguyen@thyssenkrupp.com>
2025-09-12 18:07:06 +0700

    UPDAYE

--- src/components/SelectionFilter.js
+++ src/components/SelectionFilter.js
@@ -40,6 +40,13 @@ const SelectionFilter = () => {
             onClick={async () => {
               const tcapi = await WorkspaceAPI.connect(window.parent);
               const propertyNames = [
+                { Name: "BEAM" },
+                { "PropertySet.PART_POS": fieldValue },
+                { "PropertySet.NAME": fieldValue },
+                { "PropertySet.ASSEMBLY_POS": fieldValue },
+                { "PropertySet.PROFILE": fieldValue },
+                { "PropertySet.MATERIAL": fieldValue },
+                { "Tekla Common.Class": fieldValue },
                 { "PART.PART_POS": fieldValue },
                 { "ASSEMBLY.ASSEMBLY_POS": fieldValue },
                 { "PART.CLASS_ATTR": fieldValue },
@@ -48,163 +55,145 @@ const SelectionFilter = () => {
                 { "PART.PROFILE": fieldValue },
                 { "PART.GUID": fieldValue },
                 { "ASSEMBLY.GUID": fieldValue },
-                { "IfcMaterial.Material": fieldValue },
-                { "Material.Name": fieldValue },
-                { "IfcBeamType.Name": fieldValue },
-                { "Tekla Common.Class": fieldValue },
+                { "TeklaAssembly.Assembly/Cast unit Mark": fieldValue },
               ];
-              var objectProperties = [];
-              for (const item of propertyNames) {
-                const objectIds = await tcapi.viewer.getObjects({
-                  parameter: {
-                    properties: { ...item },
-                  },
+              const modelObjects = await tcapi.viewer.getObjects({
+                parameter: {
+                  class: ["IFCBEAM", "IFCMEMBER", "IFCCOLUMN"],
+                },
+              });
+              console.log(modelObjects);
+              modelObjects.forEach(async (x) => {
+                x.objects.forEach(async (o) => {
+                  try {
+                    const properties = await tcapi.viewer.getObjectProperties(
+                      x.modelId,
+                      [o.id]
+                    );
+                    console.log(properties);
+                  } catch {}
                 });
-                objectIds.forEach(async (x) => {
-                  const ids = x.objects.map((x) => x.id);
-                  if (ids.length === 0) return;
-                  const properties = await tcapi.viewer.getObjectProperties(
-                    x.modelId,
-                    [...ids]
-                  );
-                  for (const k of properties) {
-                    console.log(k);
-                    let position = "";
-                    let key = "";
-                    let name = k.product.name;
-                    let partPos = "";
-                    let assemblyPos = "";
-                    let profile = "";
-                    let material = "";
-                    let weight = 0;
-                    let length = 0;
-                    for (const g of k.properties) {
-                      for (const f of g.properties) {
-                        if (f.name.toUpperCase().includes("GUID") && key === "")
-                          key = f.value;
-                        if (
-                          f.name.toUpperCase().includes("NAME") &&
-                          name === ""
-                        )
-                          name = f.value;
-                        if (
-                          f.name.toUpperCase().includes("PART_POS") &&
-                          partPos === ""
-                        )
-                          partPos = f.value;
-                        if (
-                          f.name.toUpperCase().includes("ASSEMBLY_POS") &&
-                          assemblyPos === ""
-                        ) {
-                          assemblyPos = f.value;
-                          position = f.value;
-                        }
-                        if (
-                          f.name.toUpperCase().includes("MAINPART.PROFILE") &&
-                          profile === ""
-                        )
-                          profile = f.value;
-                        if (
-                          f.name.toUpperCase().includes("WEIGHT") &&
-                          weight === ""
-                        )
-                          weight = f.value;
-                        if (
-                          f.name.toUpperCase().includes("LENGTH") &&
-                          length === ""
-                        )
-                          length = f.value;
-                      }
-                    }
-                    console.log(name)
+              });
+              // for (const index in allObjIds) {
+              //   const aaa = await tcapi.viewer.getObjectProperties(
+              //     allObjIds[index].modelId,
+              //     allObjIds[index].objects
+              //   );
+              //   console.log(aaa);
+              // }
 
-                    objectProperties.push({
-                      key: key,
-                      label: `${position}`,
-                      children: (
-                        <>
-                          <ul
-                            tyle={{
-                              paddingLeft: "20px",
-                              textAlign: "left",
-                            }}
-                          >
-                            <li
-                              tyle={{
-                                paddingLeft: "20px",
-                                textAlign: "left",
-                              }}
-                              key="1"
-                            >{`Part Position: ${partPos}`}</li>
-                            <li
-                              tyle={{
-                                paddingLeft: "20px",
-                                textAlign: "left",
-                              }}
-                              key="2"
-                            >{`Assembly Position: ${assemblyPos}`}</li>
-                            <li
-                              tyle={{
-                                paddingLeft: "20px",
-                                textAlign: "left",
-                              }}
-                              key="3"
-                            >{`Name: ${name}`}</li>
-                            <li
-                              tyle={{
-                                paddingLeft: "20px",
-                                textAlign: "left",
-                              }}
-                              key="4"
-                            >{`Profile: ${profile}`}</li>
-                            <li
-                              tyle={{
-                                paddingLeft: "20px",
-                                textAlign: "left",
-                              }}
-                              key="5"
-                            >{`Material: ${material}`}</li>
-                            <li
-                              tyle={{
-                                paddingLeft: "20px",
-                                textAlign: "left",
-                              }}
-                              key="6"
-                            >{`Weight: ${weight}`}</li>
-                            <li
-                              tyle={{
-                                paddingLeft: "20px",
-                                textAlign: "left",
-                              }}
-                              key="7"
-                            >{`Length: ${length}`}</li>
-                          </ul>
-                        </>
-                      ),
-                    });
-                  }
-                  await tcapi.viewer.setSelection(
-                    {
-                      modelObjectIds: [
-                        {
-                          modelId: x.modelId,
-                          objectRuntimeIds: [...ids],
-                        },
-                      ],
-                    },
-                    "add"
-                  );
-                  await tcapi.viewer.setCamera({
-                    modelObjectIds: [
-                      {
-                        modelId: x.modelId,
-                        objectRuntimeIds: [...ids],
-                      },
-                    ],
-                  });
-                });
-              }
-              setProperties(objectProperties);
+              // var objectProperties = [];
+              // for (const item of propertyNames) {
+              //   const objectIds = await tcapi.viewer.getObjects({
+              //     parameter: {
+              //       properties: { ...item },
+              //     },
+              //   });
+              //   objectIds.forEach(async (x) => {
+              //     const ids = x.objects.map((x) => x.id);
+              //     if (ids.length === 0) return;
+              //     const properties = await tcapi.viewer.getObjectProperties(
+              //       x.modelId,
+              //       [...ids]
+              //     );
+
+              //     for (const k of properties) {
+              //       for (const g of k.properties) {
+              //         if (g.name !== "ASSEMBLY" && g.name !== "PART") continue;
+              //         const type = g.name;
+              //         var key = "";
+              //         var name = "";
+              //         var partPos = "";
+              //         var assemblyPos = "";
+              //         var profile = "";
+              //         var material = "";
+              //         var weight = 0;
+              //         var length = 0;
+              //         if (type === "ASSEMBLY") {
+              //           for (const f of g.properties) {
+              //             if (f.name === "NAME") name = f.value;
+              //             if (f.name === "PART_POS") partPos = f.value;
+              //             if (f.name === "ASSEMBLY_POS") {
+              //               assemblyPos = f.value;
+              //               key = f.value;
+              //             }
+              //             if (f.name === "MAINPART.PROFILE") profile = f.value;
+              //             if (f.name === "WEIGHT") weight = f.value;
+              //             if (f.name === "LENGTH") length = f.value;
+              //           }
+              //         } else if (type == "PART") {
+              //           for (const f of g.properties) {
+              //             if (f.name === "NAME") name = f.value;
+              //             if (f.name === "PART_POS") {
+              //               key = f.value;
+              //               partPos = f.value;
+              //             }
+              //             if (f.name === "ASSEMBLY_POS") assemblyPos = f.value;
+              //             if (f.name === "PROFILE") profile = f.value;
+              //             if (f.name === "WEIGHT") weight = f.value;
+              //             if (f.name === "LENGTH") length = f.value;
+              //             if (f.name === "MATERIAL") material = f.value;
+              //           }
+              //         }
+              //         const index1 = objectProperties.findIndex(
+              //           (x) =>
+              //             x.type === "ASSEMBLY" && x.assemblyPos === assemblyPos
+              //         );
+              //         const index2 = objectProperties.findIndex(
+              //           (x) => x.type === "PART" && x.partPos === partPos
+              //         );
+
+              //         if (
+              //           (index1 < 0 && type === "ASSEMBLY") ||
+              //           (index2 < 0 && type === "PART")
+              //         ) {
+              //           objectProperties.push({
+              //             key: partPos + assemblyPos,
+              //             label: `${type}: ${key}`,
+              //             children: (
+              //               <>
+              //                 <ul
+              //                   tyle={{
+              //                     paddingLeft: "20px",
+              //                     textAlign: "left",
+              //                   }}
+              //                 >
+              //                   <li key="1">{`Part Position: ${partPos}`}</li>
+              //                   <li key="2">{`Assembly Position: ${assemblyPos}`}</li>
+              //                   <li key="3">{`Name: ${name}`}</li>
+              //                   <li key="4">{`Profile: ${profile}`}</li>
+              //                   <li key="5">{`Material: ${material}`}</li>
+              //                   <li key="6">{`Weight: ${weight}`}</li>
+              //                   <li key="7">{`Length: ${length}`}</li>
+              //                 </ul>
+              //               </>
+              //             ),
+              //           });
+              //         }
+              //       }
+              //     }
+              //     await tcapi.viewer.setSelection(
+              //       {
+              //         modelObjectIds: [
+              //           {
+              //             modelId: x.modelId,
+              //             objectRuntimeIds: [...ids],
+              //           },
+              //         ],
+              //       },
+              //       "add"
+              //     );
+              //     await tcapi.viewer.setCamera({
+              //       modelObjectIds: [
+              //         {
+              //           modelId: x.modelId,
+              //           objectRuntimeIds: [...ids],
+              //         },
+              //       ],
+              //     });
+              //   });
+              // }
+              // setProperties(objectProperties);
             }}
           >
             Select
